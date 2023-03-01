5ba16693b80fb1375420f22b9c115917b18dbd09
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-03-01 16:21:00 +0200

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+  const backgroundColor = getRandomHexColor();
+  refs.body.style.backgroundColor = backgroundColor;
+}
+function onStopBtnClick(e) {}

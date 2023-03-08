c45a59ee3a90cb755725bb3692d3dd145c70ea20
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-03-08 22:25:34 +0200

    update json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.getElementById('search-box'),
+};
+
+refs.input.addEventListener('input', onInputChange);
+
+let userInput = '';
+
+function onInputChange(e) {
+  console.log(e.target.value);
+  userInput = e.target.value;
+  fetch(`https://restcountries.com/v3.1/name/${userInput}`);
+}
+
+console.log(userInput);

a2b16f273fbcc6fb7bff1a968f0cae0238de4160
IG00RA <113199658+IG00RA@users.noreply.github.com>
2023-03-11 11:08:19 +0000

    Deploying to gh-pages from @ IG00RA/goit-js-hw-10@5038d6b9b6f6ad82637ae339eef3978f186848fc 🚀

--- index.1a138ac0.js
+++
@@ -1,2 +0,0 @@
-!function(){({input:document.getElementById("search-box")}).input.addEventListener("input",(function(e){console.log(e.target.value),t=e.target.value,fetch("https://restcountries.com/v3.1/name/".concat(t))}));var t="";console.log(t)}();
-//# sourceMappingURL=index.1a138ac0.js.map
--- index.4a8999cf.js
+++
@@ -1,2 +0,0 @@
-({input:document.getElementById("search-box")}).input.addEventListener("input",(function(t){console.log(t.target.value),e=t.target.value,fetch(`https://restcountries.com/v3.1/name/${e}`)}));let e="";console.log(e);
-//# sourceMappingURL=index.4a8999cf.js.map
